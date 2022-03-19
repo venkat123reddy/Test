aa8a47592df440797667e5b91c3ed3a745cf4083
venkat123reddy <venkatreddyattala12345@gmail.com>
2022-03-19 23:01:26 +0530

    RRR

---
+++ JenkinsRRR/src/test/java/com/example/demo/JenkinsRrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsRrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
